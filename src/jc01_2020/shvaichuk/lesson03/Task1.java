833cf6408caa47d048b744c751d2eb5d998a73bb
fstk1337 <vit.shvaichuk@gmail.com>
2020-01-27 21:26:00 +0300

    Initial Commit

--- src/jc01_2020/shvaichuk/lesson03/Task1.java
+++ src/jc01_2020/shvaichuk/lesson03/Task1.java
@@ -15,5 +15,16 @@ public class Task1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		long input = scanner.nextLong();
+		long temp;
+		long maxDigit = 0;
+        while (input >= 10) {
+            temp = input % 10;
+            input = (input - temp) / 10;
+            if (temp >= maxDigit)
+                maxDigit = temp;
+        }
+        if (input > maxDigit)
+            maxDigit = input;
+        System.out.println(maxDigit);
 	}
 }
