0b4907a6f881f4ea9fc48568fb057cf830281510
fstk1337 <vit.shvaichuk@gmail.com>
2020-01-30 21:08:18 +0300

    Tasks 6 and 7 solved.

--- src/jc01_2020/shvaichuk/lesson03/Task7.java
+++ src/jc01_2020/shvaichuk/lesson03/Task7.java
@@ -14,5 +14,14 @@ public class Task7 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int length = scanner.nextInt();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i == j || i == length - j - 1)
+                    System.out.print("x");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
 	}
 }
