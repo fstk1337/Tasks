9f16b486fbca1dbd852257e561a1f9aee2ddf21d
fstk1337 <vit.shvaichuk@gmail.com>
2020-01-30 18:36:36 +0300

    Task8 solved.

--- src/jc01_2020/shvaichuk/lesson03/Task8.java
+++ src/jc01_2020/shvaichuk/lesson03/Task8.java
@@ -10,6 +10,10 @@ package jc01_2020.shvaichuk.lesson03;
 
 public class Task8 {
 	public static void main(String[] args) {
-//        System.out.println("");
+	    for (int i = 1; i < 10; i++) {
+	        for (int j = 1; j < 10; j++) {
+                System.out.println(i + " " + "x" + " " + j + " = " + i * j);
+            }
+        }
 	}
 }
