76f568b23162f13aff75f193d64d78f966184c9b
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-08 22:34:03 +0300

    lesson 05 Tasks3-5 solved.

--- src/jc01_2020/shvaichuk/lesson05/Task4.java
+++ src/jc01_2020/shvaichuk/lesson05/Task4.java
@@ -21,5 +21,37 @@ public class Task4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String str = scanner.nextLine();
+        String numsString = "";
+        int numsCount = 0;
+		for (int i = 0; i < str.length(); i++) {
+			char chr = str.charAt(i);
+			if (chr >= '0' && chr <= '9' || chr == ' ') {
+                numsString += chr;
+				if (i == str.length() - 1 && chr != ' ' || chr != ' ' && str.charAt(i + 1) < '0' || chr != ' ' && str.charAt(i + 1) > '9') {
+					numsCount++;
+					numsString += ' ';
+				}
+			}
+		}
+		int[] numbers = new int[numsCount];
+		String numToAdd = "";
+		numsCount = 0;
+		for (int i = 0; i < numsString.length(); i++) {
+			char chr = numsString.charAt(i);
+			if (chr != ' ') {
+                numToAdd += chr;
+				if (i == numsString.length() - 1 || numsString.charAt(i + 1) == ' ') {
+					numbers[numsCount++] = Integer.parseInt(numToAdd);
+					numToAdd = "";
+				}
+			} else if (!numToAdd.equals("")) {
+				numbers[numsCount++] = Integer.parseInt(numToAdd);
+				numToAdd = "";
+			}
+		}
+		
+		for (int num : numbers) {
+			System.out.println(num);
+		}
 	}
 }
