57f370aae3c3adc4fdd664f12fc983479423efe1
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-08 22:34:03 +0300

    lesson 05 Tasks3-5 solved.

--- src/jc01_2020/shvaichuk/lesson05/Task5.java
+++ src/jc01_2020/shvaichuk/lesson05/Task5.java
@@ -16,5 +16,20 @@ public class Task5 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String str = scanner.nextLine();
+		StringBuilder sb = new StringBuilder(str);
+		for (int i = 1; i < sb.length(); i++) {
+			if (sb.charAt(i) == ' ') {
+				sb.replace(i, i + 1, "");
+				i = i - 1;
+				continue;
+			}
+			for (int j = 0; j < i; j++) {
+				if (sb.charAt(i) == sb.charAt(j)) {
+					sb.replace(i, i + 1, "");
+					i = i - 1;
+				}
+			}
+		}
+		System.out.println(sb);
 	}
 }
