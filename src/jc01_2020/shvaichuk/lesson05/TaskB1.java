c8caefc0e0e5914ebacb61c87c26ecdfc7c535a4
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-10 18:37:59 +0300

    Some changes to TaskB1 and TaskB2.

--- src/jc01_2020/shvaichuk/lesson05/TaskB1.java
+++ src/jc01_2020/shvaichuk/lesson05/TaskB1.java
@@ -17,15 +17,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB1 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
 
-		String regexp = "^[a-zA-Z0-9]+[\\w\\.-]+[\\w&&[^_]]{1}@{1}[a-zA-Z]+\\.{1}[a-zA-Z]+";
+        String regexp = "^[a-zA-Z0-9]+[\\w\\.-]+[\\w&&[^_]]{1}[.]?@{1}[a-zA-Z]+\\.{1}[a-zA-Z]+";
 
-		Pattern pattern = Pattern.compile(regexp);
-		Matcher matcher = pattern.matcher(str);
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(str);
 
-		System.out.println(matcher.find());
-	}
+        System.out.println(matcher.find());
+    }
 }
