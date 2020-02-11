fc62831172f9cd3307a34ed9ad122b553d6e483e
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-11 23:16:42 +0300

    lesson 06 Task01 completed.

--- src/jc01_2020/shvaichuk/lesson06/task01/Application.java
+++ src/jc01_2020/shvaichuk/lesson06/task01/Application.java
@@ -4,27 +4,18 @@ public class Application {
 
     public static void main(String[] args) {
         Time time1 = new Time((byte) 5, (byte) 3, (byte) 1);
-        Time time2 = new Time((byte) 3, (byte) 7, (byte) 4);
-        Time time3 = new Time((byte) 5, (byte) 3, (byte) 1);
-        Time time4 = new Time((byte) 5, (byte) 63, (byte) 0);
-        Time time5 = new Time((byte) 5, (byte) 3, (byte) 0);
+        Time time2 = new Time((byte) 7, (byte) 7, (byte) 4);
+        Time time3 = new Time((byte) 2, (byte) 4, (byte) 0);
+        Time time4 = new Time(3785);
 
-        System.out.println(time1.getFullSeconds());
-        System.out.println(time2.getFullSeconds());
-        System.out.println(time3.getFullSeconds());
-        System.out.println(time4.getFullSeconds());
-        System.out.println(time5.getFullSeconds());
-
-        System.out.println(time1.compareTo(time2));
-        System.out.println(time1.compareTo(time3));
-        System.out.println(time2.compareTo(time3));
-        System.out.println(time1.compareTo(time4));
-        System.out.println(time1.compareTo(time5));
-
-        Time time6 = new Time(3785);
-        System.out.println(time6.seconds);
-        System.out.println(time6.minutes);
-        System.out.println(time6.hours);
+        time1.printTime();
+        time2.printTime();
+        time3.printTime();
+        time4.printTime();
+	
+		System.out.println(time1.compareTo(time2));
+		System.out.println(time1.compareTo(time3));
+		System.out.println(time1.compareTo(time4));
     }
 
 }
--- src/jc01_2020/shvaichuk/lesson06/task01/Time.java
+++ src/jc01_2020/shvaichuk/lesson06/task01/Time.java
@@ -1,41 +1,37 @@
 package jc01_2020.shvaichuk.lesson06.task01;
 
 public class Time {
-    byte seconds;
-    byte minutes;
-    byte hours;
-
-    public Time() {
-
-    }
-
-    public Time(byte seconds, byte minutes, byte hours) {
-        this.seconds = seconds;
-        this.minutes = minutes;
-        this.hours = hours;
-    }
-
-    public Time(int fullSeconds) {
-        this.hours = (byte) (fullSeconds / 3600);
-        fullSeconds = fullSeconds % 3600;
-        this.minutes = (byte) (fullSeconds / 60);
-        this.seconds = (byte) (fullSeconds % 60);
-    }
-
-    public int getFullSeconds() {
-        return seconds + minutes * 60 + hours * 3600;
-    }
-
-    /*public boolean compareTo(Time time) {
-        if (this.seconds == time.seconds && this.minutes == time.minutes && this.hours == time.hours)
-            return true;
-        else
-            return false;
-    }*/
-    public boolean compareTo(Time time) {
-        if (this.getFullSeconds() == time.getFullSeconds())
-            return true;
-        else
-            return false;
-    }
+	private byte seconds;
+	private byte minutes;
+	private byte hours;
+	
+	public Time(byte seconds, byte minutes, byte hours) {
+		this.seconds = seconds;
+		this.minutes = minutes;
+		this.hours = hours;
+	}
+	
+	public Time(int fullSeconds) {
+		this.hours = (byte) (fullSeconds / 3600);
+		fullSeconds = fullSeconds % 3600;
+		this.minutes = (byte) (fullSeconds / 60);
+		this.seconds = (byte) (fullSeconds % 60);
+	}
+	
+	public int getFullSeconds() {
+		return seconds + minutes * 60 + hours * 3600;
+	}
+	
+	public int compareTo(Time time) {
+		int result = 0;
+		if (this.getFullSeconds() > time.getFullSeconds())
+			return 1;
+		 else if (this.getFullSeconds() < time.getFullSeconds())
+			return -1;
+		return result;
+	}
+	
+	public void printTime() {
+		System.out.println(String.format("%02dh:%02dm:%02ds", hours, minutes, seconds));
+	}
 }
