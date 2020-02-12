e8af5e8070bc770524a6b3ea6a7ca45219dfde6c
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-12 19:16:35 +0300

    lesson 07 task01 completed.

--- src/jc01_2020/shvaichuk/lesson07/task01/Parallelepiped.java
+++ src/jc01_2020/shvaichuk/lesson07/task01/Parallelepiped.java
@@ -1,9 +1,15 @@
 package jc01_2020.shvaichuk.lesson07.task01;
 
-public class Parallelepiped {
+public class Parallelepiped extends Shape {
 
 	private double length;
 	private double width;
 	private double height;
-
+	
+	public Parallelepiped(double length, double width, double height) {
+		this.length = length;
+		this.width = width;
+		this.height = height;
+		setVolume(length * width * height);
+	}
 }
