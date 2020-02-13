43e09074e875589277f06184cf98fa149a5d7389
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-13 20:26:19 +0300

    lesson07 task02 - knight first attempt.

--- src/jc01_2020/shvaichuk/lesson07/task02/Knight.java
+++ src/jc01_2020/shvaichuk/lesson07/task02/Knight.java
@@ -11,8 +11,23 @@ public class Knight extends Figure {
 		if (getIntFromVertical(toVertical) < 1 || getIntFromVertical(toVertical) > 8) return false;
 		if (toHorizontal < 1 || toHorizontal > 8) return false;
 		
+        int[][] allowedFields = {   {getIntFromVertical(getVertical()) - 2, getHorizontal() + 1},
+                                    {getIntFromVertical(getVertical()) - 1, getHorizontal() + 2},
+                                    {getIntFromVertical(getVertical()) + 1, getHorizontal() + 2},
+                                    {getIntFromVertical(getVertical()) + 2, getHorizontal() + 1},
+                                    {getIntFromVertical(getVertical()) - 2, getHorizontal() - 1},
+                                    {getIntFromVertical(getVertical()) - 1, getHorizontal() - 2},
+                                    {getIntFromVertical(getVertical()) + 1, getHorizontal() - 2},
+                                    {getIntFromVertical(getVertical()) + 2, getHorizontal() - 1}
+                                };
+        System.out.println(allowedFields[0][0]);
+        System.out.println(allowedFields[0][1]);
 
-		
+        for (int i = 0; i < allowedFields[0].length; i++) {
+            if (allowedFields[i][0] == getIntFromVertical(toVertical) && allowedFields[i][1] == toHorizontal)
+                return true;
+        }
+
 		return false;
 	}
 }
