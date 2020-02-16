fe1f0d74b40b719383f32fd1a307a0473331f6f7
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-16 19:51:36 +0300

    lesson07 task02 checking input optimized.

--- src/jc01_2020/shvaichuk/lesson07/task02/King.java
+++ src/jc01_2020/shvaichuk/lesson07/task02/King.java
@@ -7,9 +7,7 @@ public class King extends Figure {
 	}
 	
 	public boolean moveTo(char toVertical, int toHorizontal) {
-		if (toVertical == getVertical() && toHorizontal == getHorizontal()) return false;
-		if (getIntFromVertical(toVertical) < 1 || getIntFromVertical(toVertical) > 8) return false;
-		if (toHorizontal < 1 || toHorizontal > 8) return false;
+		if (inputIsInvalid(toVertical, toHorizontal)) return false;
 		
 		if (getIntFromVertical(toVertical) <= getIntFromVertical(getVertical()) + 1 && getIntFromVertical(toVertical) >= getIntFromVertical(getVertical()) - 1 && toHorizontal <= getHorizontal() + 1 && toHorizontal >= getHorizontal() - 1) return true;		
 		
--- src/jc01_2020/shvaichuk/lesson07/task02/Queen.java
+++ src/jc01_2020/shvaichuk/lesson07/task02/Queen.java
@@ -7,12 +7,9 @@ public class Queen extends Figure {
 	}
 	
 	public boolean moveTo(char toVertical, int toHorizontal) {
-		if (toVertical == getVertical() && toHorizontal == getHorizontal()) return false;
-		if (getIntFromVertical(toVertical) < 1 || getIntFromVertical(toVertical) > 8) return false;
-		if (toHorizontal < 1 || toHorizontal > 8) return false;
+		if (inputIsInvalid(toVertical, toHorizontal)) return false;
 		
 		if (new Bishop(getVertical(), getHorizontal()).moveTo(toVertical, toHorizontal) || new Rook(getVertical(), getHorizontal()).moveTo(toVertical, toHorizontal)) return true;
-		
 		
 		return false;
 	}
