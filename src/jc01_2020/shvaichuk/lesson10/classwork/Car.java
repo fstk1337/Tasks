da1221ce3966183f21d2f8a9d47c90ec19e51605
fstk1337 <vit.shvaichuk@gmail.com>
2020-02-20 21:28:42 +0300

    lesson10 in-class practice added.

---
+++ src/jc01_2020/shvaichuk/lesson10/classwork/Car.java
@@ -0,0 +1,20 @@
+package jc01_2020.shvaichuk.lesson10.classwork;
+
+public class Car {
+    private Model model;
+    private int size;
+
+    public Car(Model model, int size) {
+        this.model = model;
+        this.size = size;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+ }
---
+++ src/jc01_2020/shvaichuk/lesson10/classwork/Model.java
@@ -0,0 +1,7 @@
+package jc01_2020.shvaichuk.lesson10.classwork;
+
+public enum Model {
+    VW,
+    OPEL,
+    PORSCHE
+}
