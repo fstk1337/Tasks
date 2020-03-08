b36d0e9979f9e8b3c5e75988c9926b943a03a504
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-08 11:08:01 +0300

    lesson09 task02 edited.

--- src/jc01_2020/shvaichuk/lesson09/task02/Application.java
+++ src/jc01_2020/shvaichuk/lesson09/task02/Application.java
@@ -7,17 +7,19 @@ package jc01_2020.shvaichuk.lesson09.task02;
  *
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		// Объявить список
+		List<String> list = new ArrayList<>();
 		for (int i = 0; i < 5; i++) {
 			String str = scanner.nextLine();
-			// Заполнить список
+			list.add(0, str);
 		}
+		System.out.println(list.get(0));
 	}
-
 }
