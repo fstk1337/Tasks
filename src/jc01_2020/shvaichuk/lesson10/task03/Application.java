355c93d2d8c87a5447561c070af9ab22841df884
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-08 21:49:51 +0300

    lesson10 task03 solved.

--- src/jc01_2020/shvaichuk/lesson10/task03/Application.java
+++ src/jc01_2020/shvaichuk/lesson10/task03/Application.java
@@ -8,13 +8,29 @@ package jc01_2020.shvaichuk.lesson10.task03;
  *
  */
 
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
+		String str = scanner.nextLine().toLowerCase();
+		Set<String> set = new LinkedHashSet<>();
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ' ') {
+				set.add(String.valueOf(str.charAt(i)));
+			}
+		}
+		ArrayList<String> collect = set.stream().sorted()
+												.collect(Collectors.toCollection(ArrayList::new));
+		for (int i = 0; i < 5; i++) {
+			System.out.println(collect.get(i));
+		}
 	}
 
 }
