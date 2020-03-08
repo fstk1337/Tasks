179f1948521698c990fb3dba2df74d431404345e
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-08 11:53:33 +0300

    lesson09 task05 solved.

--- src/jc01_2020/shvaichuk/lesson09/task05/Application.java
+++ src/jc01_2020/shvaichuk/lesson09/task05/Application.java
@@ -8,17 +8,35 @@ package jc01_2020.shvaichuk.lesson09.task05;
  *
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		// Объявить список
-		for (int i = 0; i < 5; i++) {
-			String str = scanner.nextLine();
-			// Заполнить список
-		}
-	}
+		List<Integer> list1 = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		List<Integer> list3 = new ArrayList<>();
 
+		for (int i = 0; i < 5; i++) {
+			String str = scanner.next();
+			list1.add(Integer.parseInt(str));
+		}
+
+		for (int i = 0; i < 5; i++) {
+			String str = scanner.next();
+			list2.add(Integer.parseInt(str));
+		}
+
+		list1.removeIf(n -> n < 0);
+		list2.removeIf(n -> n > 0);
+
+		list3.addAll(list1);
+		list3.addAll(list2);
+		list3.removeIf(n -> n == 0);
+
+		System.out.println(list3);
+	}
 }
