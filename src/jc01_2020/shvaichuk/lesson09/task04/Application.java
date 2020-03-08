270806ab30fbff2634aac5bf68871e42727f0130
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-08 11:35:33 +0300

    lesson09 task04 solved.

--- src/jc01_2020/shvaichuk/lesson09/task04/Application.java
+++ src/jc01_2020/shvaichuk/lesson09/task04/Application.java
@@ -9,17 +9,29 @@ package jc01_2020.shvaichuk.lesson09.task04;
  *
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		// Объявить список
+		List<Integer> numbers = new ArrayList<>();
+		int countEven = 0;
+		int sumOfEven = 0;
 		for (int i = 0; i < 10; i++) {
 			Integer numeric = scanner.nextInt();
-			// Заполнить список
+			if (numeric <= 0) {
+				continue;
+			}
+			numbers.add(numeric);
+			if (numeric % 2 == 0) {
+				countEven++;
+				sumOfEven += numeric;
+			}
 		}
+		System.out.println(sumOfEven * 1.0 / countEven);
 	}
 
 }
