8274c7bebf36429b2169e788bab739e560f6868e
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-15 19:51:42 +0300

    lesson12 Task1 solved.

--- src/jc01_2020/shvaichuk/lesson12/Task1.java
+++ src/jc01_2020/shvaichuk/lesson12/Task1.java
@@ -11,15 +11,21 @@ package jc01_2020.shvaichuk.lesson12;
  *
  */
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Random;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Task1 {
 	public static void main(String[] args) {
 		int start = new Random().nextInt(50);
 		System.out.println("start = " + start);
 
-		IntStream.range(start, start + 40); // place code here
+		IntStream.range(start, start + 40)
+				.filter(i -> i % 2 == 1)
+				.limit(10)
+				.forEach(System.out::println);
 	}
 
 }
