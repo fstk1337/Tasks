e4918d9518e180e75296c383be670003804234b8
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-15 22:39:47 +0300

    lesson12 Task1 removed unused import.

--- src/jc01_2020/shvaichuk/lesson12/Task1.java
+++ src/jc01_2020/shvaichuk/lesson12/Task1.java
@@ -15,7 +15,6 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.util.Random;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Task1 {
 	public static void main(String[] args) {
