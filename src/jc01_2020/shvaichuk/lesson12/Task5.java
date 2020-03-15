9813ec40a3ebbc43c6bda1915ca3a2e481940e16
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-15 23:29:11 +0300

    lesson12 Task5 started.

--- src/jc01_2020/shvaichuk/lesson12/Task5.java
+++ src/jc01_2020/shvaichuk/lesson12/Task5.java
@@ -9,7 +9,46 @@ package jc01_2020.shvaichuk.lesson12;
 
 public class Task5 {
 	public static void main(String[] args) {
-
+		Cat cat1 = new Cat("Zhulik", 10);
+		Cat cat2 = new Cat("Zefir", 7);
+		System.out.println(cat1.hasMoreLives(cat2).getName());
 	}
 
+	static class Cat implements CatComparator {
+		private String name;
+		private int livesRemain;
+
+		public Cat(String name, int livesRemain) {
+			this.name = name;
+			this.livesRemain = livesRemain;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getLivesRemain() {
+			return livesRemain;
+		}
+
+		public void setLivesRemain(int livesRemain) {
+			this.livesRemain = livesRemain;
+		}
+
+		@Override
+		public Cat hasMoreLives(Cat anotherCat) {
+			if (anotherCat.getLivesRemain() > getLivesRemain()) {
+				return anotherCat;
+			}
+			return this;
+		}
+	}
+
+	public interface CatComparator {
+		Cat hasMoreLives(Cat anotherCat);
+	}
 }
