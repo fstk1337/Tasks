2ea0536a37979f4df35c5f0c2b6578fca55590f1
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-15 21:46:27 +0300

    lesson12 Task3 solved.

--- src/jc01_2020/shvaichuk/lesson12/Task3.java
+++ src/jc01_2020/shvaichuk/lesson12/Task3.java
@@ -10,18 +10,65 @@ package jc01_2020.shvaichuk.lesson12;
  *
  */
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Task3 {
 	public static void main(String[] args) {
 
 		Map<String, String> map = new HashMap<>();
-		// full the map
+		List<String> lastNames = Arrays.asList(
+				"Верясова", "Якупов", "Рыжова", "Покалюк", "Мандрыкин", "Колвецкий", "Молочинский", "Азаренков", "Ягунов",
+				"Кочерёжкина", "Орела", "Бармыкина", "Проскура", "Будаев", "Гусарова", "Тюлепова", "Яшина", "Ломоносова",
+				"Долженко", "Кочкорбаева");
+		List<String> firstNames = Arrays.asList(
+				"Дарья", "Давид" , "Алиса" , "Анатолий", "Валентин", "Харитон", "Фадей", "Всеволод", "Гаврила", "Софья",
+				"Виктория", "Полина", "Гавриил", "Анатолий", "Ираида", "Валентина", "Альбина", "Инна", "Изольда", "Вероника"
+		);
 
-		String longName = null; // use map here: longName = map...
+		for (int i = 0; i < lastNames.size(); i++) {
+			map.put(lastNames.get(i), firstNames.get(i));
+		}
 
-		System.out.println("longName = " + longName);
+		System.out.println(map.values()); //1
+
+		map.forEach((lastName, firstName) -> map.put(lastName, firstName + " " + lastName)); //2
+
+		String longName = String.valueOf(map.values().stream()
+				.filter(s -> s.length() <= 16) //3
+				.reduce((s1, s2) -> {
+					if (s2.length() > s1.length()) {
+						return s2;
+					} else {
+						return s1;
+					}
+				})
+		);
+
+		System.out.println("longName = " + longName); //4
 
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
