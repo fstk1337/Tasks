f3e85291f7edccfc5983cb299de390a584503edf
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-16 19:56:44 +0300

    lesson10 task05 started.

--- src/jc01_2020/shvaichuk/lesson10/task05/Application.java
+++ src/jc01_2020/shvaichuk/lesson10/task05/Application.java
@@ -12,13 +12,33 @@ package jc01_2020.shvaichuk.lesson10.task05;
  *
  */
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Application {
 
 	public static void main(String[] args) {
 		// Объявить список
+        Map<String, Map<String, Integer>> map = new TreeMap<>();
+
+        Scanner sc = new Scanner(System.in);
+
 		for (int i = 0; i < 5; i++) {
 			// Заполнить список
+            String input = sc.nextLine();
+            String[] split = input.split(" ");
+            String name = split[0];
+            String good = split[1];
+            int quantity = Integer.valueOf(split[2]);
+
+            Map<String, Integer> goods = new TreeMap<>();
+            goods.put(good, quantity);
+
+            map.put(name, goods);
 		}
+
+
 	}
 
 }
