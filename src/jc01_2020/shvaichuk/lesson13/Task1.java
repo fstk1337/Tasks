0c8de6b37033bcab903c9adab1c2491c5fd9c8de
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-24 19:04:45 +0300

    lesson13 Task1 solved.

--- src/jc01_2020/shvaichuk/lesson13/Task1.java
+++ src/jc01_2020/shvaichuk/lesson13/Task1.java
@@ -7,9 +7,29 @@ package jc01_2020.shvaichuk.lesson13;
  *
  */
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class Task1 {
 	public static void main(String[] args) {
+		Random rnd = new Random();
+		LocalDate date1 = LocalDate.of(2020, 3, rnd.nextInt(4) + 1);
+		LocalDate date2 = LocalDate.of(2020, 3, rnd.nextInt(4) + 1);
 
+		System.out.println("дата1: " + date1);
+		System.out.println("дата2: " + date2);
+
+		String comparingResult = new String();
+
+		if (date1.isBefore(date2)) {
+			comparingResult = " меньше ";
+		} else if (date1.isAfter(date2)) {
+			comparingResult = " больше ";
+		} else {
+			comparingResult = " равна ";
+		}
+
+		System.out.println("дата1" + comparingResult + "дата2");
 	}
 
 }
