b9ed126234fef993e15c33fde909cf348d4cac27
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-24 20:01:45 +0300

    lesson13 Task2 solved.

--- src/jc01_2020/shvaichuk/lesson13/Task2.java
+++ src/jc01_2020/shvaichuk/lesson13/Task2.java
@@ -7,8 +7,25 @@ package jc01_2020.shvaichuk.lesson13;
  *
  */
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Task2 {
 	public static void main(String[] args) {
+		ZonedDateTime currentDate = ZonedDateTime.now();
+		System.out.println(currentDate);
 
+		List<String> timeZones = new ArrayList<>(ZoneId.getAvailableZoneIds());
+
+		Random rnd = new Random();
+
+		for (int i = 0; i < 10; i++) {
+			int randomZoneNumber = rnd.nextInt(timeZones.size());
+			currentDate = currentDate.withZoneSameInstant(ZoneId.of(timeZones.get(randomZoneNumber)));
+			System.out.println(currentDate);
+		}
 	}
 }
