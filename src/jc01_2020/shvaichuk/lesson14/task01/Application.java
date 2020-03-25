ddd1c6deeaa32eab5949688121211762823ddc09
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-25 20:00:03 +0300

    lesson14 task01 solved.

--- src/jc01_2020/shvaichuk/lesson14/task01/Application.java
+++ src/jc01_2020/shvaichuk/lesson14/task01/Application.java
@@ -8,9 +8,17 @@ package jc01_2020.shvaichuk.lesson14.task01;
  *
  */
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Application {
 
 	public static void main(String[] args) {
+		try (FileWriter fr = new FileWriter("src/jc01_2020/shvaichuk/lesson14/resource/lesson14-01.txt")) {
+			fr.write("Давай проверим, что мы умеем писать в файл.\nПоехали!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
