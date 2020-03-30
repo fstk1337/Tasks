b0b42e1ea3d8c9c2857df054f09d0db017ab51fc
fstk1337 <vit.shvaichuk@gmail.com>
2020-03-30 06:23:05 +0300

    lesson14 task02 solved.

--- src/jc01_2020/shvaichuk/lesson14/task02/Application.java
+++ src/jc01_2020/shvaichuk/lesson14/task02/Application.java
@@ -6,10 +6,35 @@ package jc01_2020.shvaichuk.lesson14.task02;
  *
  */
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("src/jc01_2020/shvaichuk/lesson14/resource/lesson14-01.txt");
+		FileInputStream fis = new FileInputStream("src/jc01_2020/shvaichuk/lesson14/resource/lesson14-01.txt");
 
+
+		String content1 = "";
+		Scanner sc = new Scanner(fr);
+		content1 += sc.nextLine();
+		while (sc.hasNextLine()) {
+			content1 += "\n";
+			content1 += sc.nextLine();
+		}
+		fr.close();
+		sc.close();
+
+
+		byte[] buffer = new byte[fis.available()];
+		fis.read(buffer, 0, buffer.length);
+		String content2 = new String(buffer);
+
+		System.out.println(content1);
+		System.out.println(content2);
+
+		System.out.println(content1.equals(content2));
 	}
 
 }
